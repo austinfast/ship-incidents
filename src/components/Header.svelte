bab9e411b93d67798d428c35283d17c8eb1467ed
mitchthorson <mthorson@usatoday.com>
2020-01-08 10:30:52 -0500

    update header component

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,5 +1,15 @@
+<style>
+  .section-header {
+    margin-bottom: 2rem;
+  }
+  h1.page-headline {
+    font-size: 2.2rem;
+    font-weight: 900;
+  }
+</style>
 <section class="section-header">
   <header>
-    <h1>Header</h1>
+    <h1 class="page-headline">Headline TK</h1>
+    <p class="header-text">Last Updated: </p>
   </header>
 </section>
