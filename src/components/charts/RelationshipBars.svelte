bb32b00163f6f37b7fc54ec3c90ca7f24a07c826
mitchthorson <mthorson@usatoday.com>
2020-02-20 12:21:33 -0500

    initial setup of relationship bar component

---
+++ src/components/charts/RelationshipBars.svelte
@@ -0,0 +1,9 @@
+<script>
+  let wrapEl;
+  let svgEl;
+</script>
+<div class="relationship-bar-wrapper" bind:this={wrapEl}>
+  <svg class="relationship-bar" bind:this={svgEl}>
+    
+  </svg>
+</div>
