779215f94842ec8f2a8302ef2283bd52469ae7eb
mitchthorson <mthorson@usatoday.com>
2021-04-21 14:28:35 -0400

    update

--- scripts/fetch_data.js
+++ scripts/fetch_data.js
@@ -5,7 +5,6 @@ const mapshaper = require("mapshaper");
 const reproject = require("dirty-reprojectors");
 const projections = require("dirty-reprojectors/projections");
 
-
 // list of the final fields included in the victims data
 const victim_fields = ["relationshipcat", "age"];
 
@@ -14,59 +13,90 @@ const offender_fields = ["sex", "age"];
 
 // function to grab necessary environment variables
 function get_auth() {
-  const email = process.env.MASS_KILLINGS_DB_EMAIL || null;
-  const password = process.env.MASS_KILLINGS_DB_PASSWORD || null;
-  if (!email || !password) {
-    console.warn("Credentials not found. Please make sure $MASS_KILLINGS_DB_EMAIL and $MASS_KILLINGS_DB_PASSWORD are set in your environment.");
-    process.exit(1);
-  }
-  return {
-    email,
-    password
-  };
+	const email = process.env.MASS_KILLINGS_DB_EMAIL || null;
+	const password = process.env.MASS_KILLINGS_DB_PASSWORD || null;
+	if (!email || !password) {
+		console.warn("Credentials not found. Please make sure $MASS_KILLINGS_DB_EMAIL and $MASS_KILLINGS_DB_PASSWORD are set in your environment.");
+		process.exit(1);
+	}
+	return {
+		email,
+		password,
+	};
 }
 
 // function to categorize victims relationships in to clean categories
 function format_victims(victims) {
-  const family = ["Sibling", "Spouse", "Cousin", "Child (including step)", "Parent", "Niece/Nephew", "Other familial relationship", "Aunt/Uncle", "Relative in law", "Grandparent", "Grandchild", "Relative or in-law", "Child or stepchild", "Parent or stepparent", "Ex dating relationship", "Dating relationship", "Ex spouse", "Ex relative in law"];
-  const acquaintance = ["Neighbor", "Coworker or employer", "Classmate", "Roommate", "Friend",  "Co-worker or employer", "Criminal associate", "Individual with some non-blood/marriage relationship to a known person", "Relative of a known person", "Other", "Acquaintence"];
-  const first_responder = ["First responder"];
-  const stranger = ["Random bystander/stranger"];
-  const unknown = ["Unknown"]
-  return victims.map((victim) => {
-    let relationshipcat;
-    if (family.indexOf(victim.vorelationship) >= 0) {
-      relationshipcat = "family";
-    } else if (acquaintance.indexOf(victim.vorelationship) >= 0) {
-      relationshipcat = "acquaintance"
-    } else if (first_responder.indexOf(victim.vorelationship) >= 0) {
-      relationshipcat = "first_responder"
-    } else if (stranger.indexOf(victim.vorelationship) >= 0) {
-      relationshipcat = "stranger"
-    } else if (unknown.indexOf(victim.vorelationship >= 0)) {
-      relationshipcat = "unknown"
-    } else if (!victim.vorelationship) {
-      relationshipcat = "unknown";
-    }
-    let victim_with_category = Object.assign({}, victim, {
-      relationshipcat
-    });
-    let clean_victim = {};
-    victim_fields.forEach(field => {
-      clean_victim[field] = victim_with_category[field];
-    });
-    return clean_victim;
-  });
-} 
+	const family = [
+		"Sibling",
+		"Spouse",
+		"Cousin",
+		"Child (including step)",
+		"Parent",
+		"Niece/Nephew",
+		"Other familial relationship",
+		"Aunt/Uncle",
+		"Relative in law",
+		"Grandparent",
+		"Grandchild",
+		"Relative or in-law",
+		"Child or stepchild",
+		"Parent or stepparent",
+		"Ex dating relationship",
+		"Dating relationship",
+		"Ex spouse",
+		"Ex relative in law",
+	];
+	const acquaintance = [
+		"Neighbor",
+		"Coworker or employer",
+		"Classmate",
+		"Roommate",
+		"Friend",
+		"Co-worker or employer",
+		"Criminal associate",
+		"Individual with some non-blood/marriage relationship to a known person",
+		"Relative of a known person",
+		"Other",
+		"Acquaintence",
+	];
+	const first_responder = ["First responder"];
+	const stranger = ["Random bystander/stranger"];
+	const unknown = ["Unknown"];
+	return victims.map((victim) => {
+		let relationshipcat;
+		if (family.indexOf(victim.vorelationship) >= 0) {
+			relationshipcat = "family";
+		} else if (acquaintance.indexOf(victim.vorelationship) >= 0) {
+			relationshipcat = "acquaintance";
+		} else if (first_responder.indexOf(victim.vorelationship) >= 0) {
+			relationshipcat = "first_responder";
+		} else if (stranger.indexOf(victim.vorelationship) >= 0) {
+			relationshipcat = "stranger";
+		} else if (unknown.indexOf(victim.vorelationship >= 0)) {
+			relationshipcat = "unknown";
+		} else if (!victim.vorelationship) {
+			relationshipcat = "unknown";
+		}
+		let victim_with_category = Object.assign({}, victim, {
+			relationshipcat,
+		});
+		let clean_victim = {};
+		victim_fields.forEach((field) => {
+			clean_victim[field] = victim_with_category[field];
+		});
+		return clean_victim;
+	});
+}
 
 function format_offenders(offenders) {
-  return offenders.map((offender) => {
-    let clean_offender = {};
-    offender_fields.forEach(field => {
-      clean_offender[field] = offender[field]
-    });
-    return clean_offender;
-  });
+	return offenders.map((offender) => {
+		let clean_offender = {};
+		offender_fields.forEach((field) => {
+			clean_offender[field] = offender[field];
+		});
+		return clean_offender;
+	});
 }
 
 // output data directory
@@ -77,64 +107,74 @@ const api_data_url = "https://mka.ap.org/api/v1/all";
 
 // first, generate a fresh API token
 // http://mka.ap.org/admin/api
-axios.post(api_token_url, auth_credentials)
-  .then((response) => {
-    return response.data.token;
-  })
-  // then request new data
-  .then((token) => {
-    return axios.get(api_data_url, {
-      headers: {
-        "Authorization": token
-      }
-    })
-  })
-  // write data to json files
-  .then((response) => {
-    let all_data = response.data;
-    let incidents_w_year = all_data.incidents.map((incident) => {
-      return Object.assign({}, incident, {
-        year: incident.date ? incident.date.split("-")[0] : null
-      });
-    });
-    all_data.victims = format_victims(all_data.victims);
-    all_data.offenders = format_offenders(all_data.offenders);
-    all_data.incidents = incidents_w_year;
-    return Promise.all([
-      sander.writeFile(path.join(output_dir, "json", "all.json"), JSON.stringify(all_data)),
-      sander.writeFile(path.join(output_dir, "json", "incidents.json"), JSON.stringify(all_data.incidents)),
-      sander.writeFile(path.join(output_dir, "json", "victims.json"), JSON.stringify(all_data.victims)),
-      sander.writeFile(path.join(output_dir, "json", "offenders.json"), JSON.stringify(all_data.offenders))
-    ]);
-  })
-  // create geojson file of incidents
-  .then((d) => {
-    const command = `-i ${path.join(output_dir, "json", "incidents.json")} encoding=utf8 -filter-fields "latitude,longitude,victims,type,casename,date,city,state,id" -points x=longitude y=latitude -o ${path.join(output_dir, "json", "incidents_geo.json")} format=geojson`
-    return mapshaper.runCommands(command)
-  })
-  // load the geojson
-  .then(() => {
-    return sander.readFile(path.join(output_dir, "json", "incidents_geo.json"), {encoding: "utf8"})
-  })
-  // reproject geojson into mapbox friendly albers projection
-  .then(geometry => {
-    let geo = JSON.parse(geometry);
-    var features = geo.features;
-    let options = {
-      forward: "albersUsa",
-      reverse: 'mercator',
-      projections
-    };
-    let reprojected = features.map(feature => {
-      let reprojectedPoint = reproject(options, feature.geometry);
-      return Object.assign({}, feature, {
-        geometry: reprojectedPoint
-      });
-    });
-    geo.features = reprojected;
-    return sander.writeFile(path.join(output_dir, "json", "incidents_geo_projected.json"), JSON.stringify(geo));
-  })
-  .catch(e => {
-    console.warn(e);
-    process.exit(1);
-  });
+axios
+	.post(api_token_url, auth_credentials)
+	.then((response) => {
+		return response.data.token;
+	})
+	// then request new data
+	.then((token) => {
+		return axios.get(api_data_url, {
+			headers: {
+				Authorization: token,
+			},
+		});
+	})
+	// write data to json files
+	.then((response) => {
+		let all_data = response.data;
+		console.log(Object.keys(all_data));
+		let incidents_w_year = all_data.incidents.map((incident) => {
+			return Object.assign({}, incident, {
+				year: incident.date ? incident.date.split("-")[0] : null,
+			});
+		});
+		all_data.victims = format_victims(all_data.victims);
+		all_data.offenders = format_offenders(all_data.offenders);
+		all_data.incidents = incidents_w_year;
+		return Promise.all([
+			sander.writeFile(path.join(output_dir, "json", "all.json"), JSON.stringify(all_data)),
+			sander.writeFile(path.join(output_dir, "json", "incidents.json"), JSON.stringify(all_data.incidents)),
+			sander.writeFile(path.join(output_dir, "json", "victims.json"), JSON.stringify(all_data.victims)),
+			sander.writeFile(path.join(output_dir, "json", "offenders.json"), JSON.stringify(all_data.offenders)),
+		]);
+	})
+	// create geojson file of incidents
+	.then((d) => {
+		const command = `-i ${path.join(
+			output_dir,
+			"json",
+			"incidents.json"
+		)} encoding=utf8 -filter-fields "latitude,longitude,victims,type,casename,date,city,state,id" -points x=longitude y=latitude -o ${path.join(
+			output_dir,
+			"json",
+			"incidents_geo.json"
+		)} format=geojson`;
+		return mapshaper.runCommands(command);
+	})
+	// load the geojson
+	.then(() => {
+		return sander.readFile(path.join(output_dir, "json", "incidents_geo.json"), { encoding: "utf8" });
+	})
+	// reproject geojson into mapbox friendly albers projection
+	.then((geometry) => {
+		let geo = JSON.parse(geometry);
+		var features = geo.features;
+		let options = {
+			forward: "albersUsa",
+			reverse: "mercator",
+			projections,
+		};
+		let reprojected = features.map((feature) => {
+			let reprojectedPoint = reproject(options, feature.geometry);
+			return Object.assign({}, feature, {
+				geometry: reprojectedPoint,
+			});
+		});
+		geo.features = reprojected;
+		return sander.writeFile(path.join(output_dir, "json", "incidents_geo_projected.json"), JSON.stringify(geo));
+	})
+	.catch((e) => {
+		console.warn(e);
+		process.exit(1);
+	});
--- src/utils/data.js
+++ src/utils/data.js
@@ -4,255 +4,264 @@ import { scaleLinear, histogram, max } from "d3";
 import { urlFor } from "./urls.js";
 
 class DataManager {
-  constructor() {
-    this._data = null;
-  }
-  async getData() {
-    // this function returns all data, formatted and ready to go. Returns a cached version if data has been loaded already.
-    if (!this._data) {
+	constructor() {
+		this._data = null;
+	}
+	async getData() {
+		// this function returns all data, formatted and ready to go. Returns a cached version if data has been loaded already.
+		if (!this._data) {
+			// request data from JSON files
+			const response = await fetch(this.getDataURL("all.json"));
+			let rawData = await response.json();
+			this._data = rawData;
 
-      // request data from JSON files
-      const response = await fetch(this.getDataURL("all.json"))
-      let rawData = await response.json();
-      this._data = rawData;
+			// incident lookup object
+			this._data.incidentLookup = {};
 
-      // incident lookup object
-      this._data.incidentLookup = {};
+			// set parse and set a few additional variables for each incident
+			this._data.incidents.forEach((incident) => {
+				// a real JS date
+				incident.real_date = this.parseDate(incident.date);
 
-      // set parse and set a few additional variables for each incident
-      this._data.incidents.forEach(incident => {
+				// a string version of just the year
+				incident.year = incident.date ? incident.date.split("-")[0] : null;
 
-        // a real JS date
-        incident.real_date = this.parseDate(incident.date);
+				// an array of gun types rather than a string
+				incident.gun_type_array = this.parseGunTypes(incident.gun_type);
+				this._data.incidentLookup[incident.id] = incident;
+			});
 
-        // a string version of just the year
-        incident.year = incident.date ? incident.date.split("-")[0] : null;
+			// format timeline data
+			this._data["timeline"] = this.formatTimeline(rawData.incidents);
 
-        // an array of gun types rather than a string
-        incident.gun_type_array = this.parseGunTypes(incident.gun_type);
-        this._data.incidentLookup[incident.id] = incident;
-      });
+			// format yearly summary data
+			this._data["yearly_summaries"] = this.formatYearlySummaries(rawData.incidents);
 
-      // format timeline data
-      this._data["timeline"] = this.formatTimeline(rawData.incidents);
+			// fetch geographic data seperately
+			this._data["incidents_geo"] = await (await this.getGeoData()).json();
 
-      // format yearly summary data
-      this._data["yearly_summaries"] = this.formatYearlySummaries(rawData.incidents);
+			// count location types
+			this._data["location_type_counts"] = this.countTypes(rawData.incidents, "location_type");
 
-      // fetch geographic data seperately
-      this._data["incidents_geo"] = await (await this.getGeoData()).json();
+			// count gun types
+			this._data["gun_type_counts"] = this.countTypes(rawData.incidents, "gun_type_array");
 
-      // count location types
-      this._data["location_type_counts"] = this.countTypes(rawData.incidents, "location_type");
+			// count victim relationships
+			this._data["victim_relationship_counts"] = this.getRelationshipCounts(rawData.victims);
 
-      // count gun types
-      this._data["gun_type_counts"] = this.countTypes(rawData.incidents, "gun_type_array");
+			// victims age bins
+			this._data["victim_age_scale"] = this.getAgeScale(this._data.victims);
+			this._data["victim_binned_ages"] = this.getAgeBins(this._data.victims, this._data.victim_age_scale);
 
-      // count victim relationships
-      this._data["victim_relationship_counts"] = this.getRelationshipCounts(rawData.victims);
+			// clean offenders
+			this._data["offenders"] = this.cleanOffenders(this._data["offenders"]);
 
-      // victims age bins
-      this._data["victim_age_scale"] = this.getAgeScale(this._data.victims);
-      this._data["victim_binned_ages"] = this.getAgeBins(this._data.victims, this._data.victim_age_scale);
+			// offender age bins
+			this._data["offender_age_scale"] = this.getAgeScale(this._data.offenders);
+			this._data["offender_binned_ages"] = this.getAgeBins(this._data.offenders, this._data.offender_age_scale);
 
-      // clean offenders
-      this._data["offenders"] = this.cleanOffenders(this._data["offenders"]);
+			// offender sex
+			this._data["offender_gender_counts"] = this.countTypes(this._data.offenders, "sex");
+		}
+		return this._data;
+	}
 
-      // offender age bins
-      this._data["offender_age_scale"] = this.getAgeScale(this._data.offenders);
-      this._data["offender_binned_ages"] = this.getAgeBins(this._data.offenders, this._data.offender_age_scale);
+	async getGeoData() {
+		// function requests geographically projected data file
+		return await fetch(this.getDataURL("incidents_geo_projected.json"));
+	}
 
-      // offender sex
-      this._data["offender_gender_counts"] = this.countTypes(this._data.offenders, "sex");
-    }
-    return this._data;
-  }
+	getDataURL(filename) {
+		// function to determine the correct URL path for data files. Currently only returns relative path.
+		let url = "data/json/" + filename;
+		return urlFor(url);
+	}
 
-  async getGeoData() {
-    // function requests geographically projected data file
-    return await fetch(this.getDataURL("incidents_geo_projected.json"));
-  }
+	formatTimeline(rawIncidents) {
+		// formats incidents in a timeline frindly way, grouping by year, and simplifies data associated with incidents.
+		let incidents_by_year = {};
+		for (var i = 0; i < rawIncidents.length; i += 1) {
+			let incident = rawIncidents[i];
+			let timeline_incident = {
+				real_date: incident.real_date,
+				year: incident.year,
+				victims: incident.victims,
+				id: incident.id,
+				type: incident.type,
+				firstcod: incident.firstcod,
+			};
+			let year = incident.year;
+			if (incidents_by_year[year]) {
+				incidents_by_year[year].push(timeline_incident);
+			} else {
+				incidents_by_year[year] = [timeline_incident];
+			}
+		}
+		return incidents_by_year;
+	}
 
-  getDataURL(filename) {
-    // function to determine the correct URL path for data files. Currently only returns relative path.
-    let url = "data/json/" + filename;
-    return urlFor(url);
-  }
+	formatYearlySummaries(rawIncidents) {
+		let yearsData = [];
+		let years = Object.keys(this._data.timeline);
+		years.forEach((year) => {
+			if (year == "null") {
+				return;
+			}
+			let incidents = this._data.timeline[year];
+			let yearSummary = incidents.reduce(
+				(yearInfo, incident) => {
+					return Object.assign(yearInfo, {
+						victims: yearInfo.victims + incident.victims,
+						mass_shooting_victims:
+							incident.firstcod == "Shooting" ? yearInfo.mass_shooting_victims + incident.victims : yearInfo.mass_shooting_victims,
+						mass_public_shooting_victims:
+							incident.firstcod == "Shooting" && incident.type == "Public"
+								? yearInfo.mass_public_shooting_victims + incident.victims
+								: yearInfo.mass_public_shooting_victims,
+						numinjured: yearInfo.numinjured + incident.numinjured,
+						incidents: yearInfo.incidents + 1,
+						incidents_family: incident.type == "Family" ? yearInfo.incidents_family + 1 : yearInfo.incidents_family,
+						incidents_public: incident.type == "Public" ? yearInfo.incidents_public + 1 : yearInfo.incidents_public,
+						incidents_felony: incident.type == "Felony" ? yearInfo.incidents_felony + 1 : yearInfo.incidents_felony,
+						incidents_other: incident.type == "Other" || incident.type == "Unsolved" ? yearInfo.incidents_other + 1 : yearInfo.incidents_other,
+						mass_shootings: incident.firstcod == "Shooting" ? yearInfo.mass_shootings + 1 : yearInfo.mass_shootings,
+						mass_public_shootings:
+							incident.firstcod == "Shooting" && incident.type == "Public" ? yearInfo.mass_public_shootings + 1 : yearInfo.mass_public_shootings,
+					});
+				},
+				{
+					year,
+					year_date: this.parseDate(year + "-1-1"),
+					victims: 0,
+					mass_shooting_victims: 0,
+					mass_public_shooting_victims: 0,
+					numinjured: 0,
+					incidents: 0,
+					mass_shootings: 0,
+					mass_public_shootings: 0,
+					incidents_family: 0,
+					incidents_public: 0,
+					incidents_felony: 0,
+					incidents_other: 0,
+				}
+			);
+			yearsData.push(yearSummary);
+		});
+		return yearsData;
+	}
 
-  formatTimeline(rawIncidents) {
-    // formats incidents in a timeline frindly way, grouping by year, and simplifies data associated with incidents.
-    let incidents_by_year = {};
-    for (var i = 0; i < rawIncidents.length; i += 1) {
-      let incident = rawIncidents[i];
-      let timeline_incident = {
-        "real_date": incident.real_date,
-        year: incident.year,
-        victims: incident.victims,
-        id: incident.id,
-        type: incident.type,
-        firstcod: incident.firstcod
-      };
-      let year = incident.year;
-      if (incidents_by_year[year]) {
-        incidents_by_year[year].push(timeline_incident);
-      } else {
-        incidents_by_year[year] = [timeline_incident];
-      }
-    }
-    return incidents_by_year;
-  }
+	parseDate(dateStr) {
+		// handle null dates
+		if (!dateStr) {
+			return null;
+		}
+		let dateParts = dateStr.split("-");
+		let date = new Date(dateParts[0], parseInt(dateParts[1]) - 1, dateParts[2], 12);
+		return date;
+	}
 
-  formatYearlySummaries(rawIncidents) {
-    let yearsData = [];
-    let years = Object.keys(this._data.timeline);
-    years.forEach((year) => {
-    if(year == "null") {
-      return;
-    }
-    let incidents = this._data.timeline[year];
-    let yearSummary = incidents.reduce((yearInfo, incident) => {
-      return Object.assign(yearInfo, {
-        victims: yearInfo.victims + incident.victims,
-        mass_shooting_victims: (incident.firstcod == "Shooting") ? yearInfo.mass_shooting_victims + incident.victims : yearInfo.mass_shooting_victims,
-        mass_public_shooting_victims: (incident.firstcod == "Shooting") && incident.type == "Public" ? yearInfo.mass_public_shooting_victims + incident.victims : yearInfo.mass_public_shooting_victims,
-        numinjured: yearInfo.numinjured + incident.numinjured,
-        incidents: yearInfo.incidents + 1,
-        incidents_family: incident.type == "Family" ? yearInfo.incidents_family + 1 : yearInfo.incidents_family,
-        incidents_public: incident.type == "Public" ? yearInfo.incidents_public + 1 : yearInfo.incidents_public,
-        incidents_felony: incident.type == "Felony" ? yearInfo.incidents_felony + 1 : yearInfo.incidents_felony,
-        incidents_other: incident.type == "Other" || incident.type == "Unsolved" ? yearInfo.incidents_other + 1 : yearInfo.incidents_other,
-        mass_shootings: incident.firstcod == "Shooting" ? yearInfo.mass_shootings + 1 : yearInfo.mass_shootings,
-        mass_public_shootings: incident.firstcod == "Shooting" && incident.type == "Public" ? yearInfo.mass_public_shootings + 1 : yearInfo.mass_public_shootings
-      });
-      }, {
-        year,
-        year_date: this.parseDate(year + "-1-1"),
-        victims: 0,
-        mass_shooting_victims: 0,
-        mass_public_shooting_victims: 0,
-        numinjured: 0,
-        incidents: 0,
-        mass_shootings: 0,
-        mass_public_shootings: 0,
-        incidents_family: 0,
-        incidents_public: 0,
-        incidents_felony: 0,
-        incidents_other: 0
-      });
-      yearsData.push(yearSummary);
-    });
-    return yearsData;
-  }
+	countTypes(rawIncidents, countKey, nullKey = "Unknown") {
+		let typeLookup = {};
+		let typeArray = [];
 
-  parseDate(dateStr) {
-    // handle null dates
-    if (!dateStr) {
-      return null;
-    }
-    let dateParts = dateStr.split("-");
-    let date = new Date(dateParts[0], parseInt(dateParts[1]) - 1, dateParts[2], 12)
-    return date;
-  }
+		rawIncidents.forEach((incident) => {
+			// if the countKey value is a string count that string
+			if (typeof incident[countKey] == "string") {
+				if (typeLookup[incident[countKey]]) {
+					typeLookup[incident[countKey]] += 1;
+				} else {
+					typeLookup[incident[countKey]] = 1;
+				}
+			}
+			// if the countKey value is null, then use the nullKey to count
+			else if (incident[countKey] == null) {
+				if (typeLookup[nullKey]) {
+					typeLookup[nullKey] += 1;
+				} else {
+					typeLookup[nullKey] = 1;
+				}
+				// if the countKey value is an array, count for each item in array
+			} else if (typeof incident[countKey] == "object") {
+				incident[countKey].forEach((countCategory) => {
+					if (typeLookup[countCategory]) {
+						typeLookup[countCategory] += 1;
+					} else {
+						typeLookup[countCategory] = 1;
+					}
+				});
+			}
+		});
 
-  countTypes(rawIncidents, countKey, nullKey = "Unknown") {
-    let typeLookup = {};
-    let typeArray = []
+		for (var key in typeLookup) {
+			typeArray.push({
+				label: key,
+				count: typeLookup[key],
+			});
+		}
 
-    rawIncidents.forEach(incident => {
-      // if the countKey value is a string count that string
-      if (typeof incident[countKey] == "string") {
-        if (typeLookup[incident[countKey]]) {
-          typeLookup[incident[countKey]] += 1;
-        } else {
-          typeLookup[incident[countKey]] = 1;
-        }
-      }
-      // if the countKey value is null, then use the nullKey to count
-      else if (incident[countKey] == null) {
-        if (typeLookup[nullKey]) {
-          typeLookup[nullKey] += 1;
-        } else {
-          typeLookup[nullKey] = 1;
-        }
-      // if the countKey value is an array, count for each item in array
-      } else if (typeof incident[countKey] == "object") {
-        incident[countKey].forEach(countCategory => {
-          if (typeLookup[countCategory]) {
-            typeLookup[countCategory] += 1;
-          } else {
-            typeLookup[countCategory] = 1;
-          }
-        })
-      }
-    });
+		return typeArray.sort((a, b) => b.count - a.count);
+	}
 
-    for (var key in typeLookup) {
-      typeArray.push({
-        label: key,
-        count: typeLookup[key]
-      });
-    }
+	parseGunTypes(gun_type_string) {
+		if (!gun_type_string) {
+			return ["Unknown"];
+		}
+		let split_1 = gun_type_string.split("&");
+		if (split_1.length < 2) {
+			// if type is blank, override to "Unknown"
+			if (split_1 == "") {
+				return ["Unknown"];
+			}
+			return split_1;
+		}
+		let split_2 = split_1[0].split(",");
+		let result = split_2.concat([split_1[1]]);
+		result.forEach((item, i) => {
+			result[i] = item.trim();
+		});
+		return result;
+	}
 
-    return typeArray.sort((a,b) => b.count - a.count);
-  }
+	getRelationshipCounts(victims) {
+		return victims.reduce((allCounts, victim) => {
+			if (allCounts[victim.relationshipcat]) {
+				allCounts[victim.relationshipcat] += 1;
+			} else {
+				allCounts[victim.relationshipcat] = 1;
+			}
+			return allCounts;
+		}, {});
+	}
 
-  parseGunTypes(gun_type_string) {
-    let split_1 = gun_type_string.split("&");
-    if (split_1.length < 2) {
-      // if type is blank, override to "Unknown"
-      if (split_1 == "") {
-        return ["Unknown"];
-      }
-      return split_1;
-    }
-    let split_2 = split_1[0].split(",");
-    let result = split_2.concat([split_1[1]]);
-    result.forEach((item, i) => {
-      result[i] = item.trim();
-    });
-    return result;
-  }
+	getAgeScale(people) {
+		let people_with_ages = people.filter((person) => person.age !== null);
+		return scaleLinear()
+			.domain([0, max(people_with_ages, (d) => d.age)])
+			.range([0, 1]);
+	}
 
-  getRelationshipCounts(victims) {
-    return victims.reduce((allCounts, victim) => {
-      if (allCounts[victim.relationshipcat]) {
-        allCounts[victim.relationshipcat] += 1;
-      } else {
-        allCounts[victim.relationshipcat] = 1;
-      }
-      return allCounts;
-    }, {});
-  }
+	getAgeBins(people, age_scale) {
+		// filter out null ages
+		let people_with_ages = people.filter((person) => person.age !== null);
 
-  getAgeScale(people) {
-    let people_with_ages = people.filter(person => person.age !== null);
-    return scaleLinear()
-    .domain([0, max(people_with_ages, d => d.age)])
-    .range([0,1]);
-  }
+		let age_bins = histogram()
+			.domain(age_scale.domain())
+			.thresholds(age_scale.ticks(20))
+			.value((d) => d.age);
 
-  getAgeBins(people, age_scale) {
-    // filter out null ages
-    let people_with_ages = people.filter(person => person.age !== null);
-    
-    let age_bins = histogram()
-    .domain(age_scale.domain())
-    .thresholds(age_scale.ticks(20))
-    .value(d => d.age);
+		return age_bins(people_with_ages);
+	}
 
-    return age_bins(people_with_ages);
-  }
-
-  cleanOffenders(raw_offenders) {
-    return raw_offenders.map(offender => {
-      let clean_offender = Object.assign({}, offender);
-      // replace empty and null sex fields with "Unknown"
-      if (clean_offender.sex == "" || clean_offender.sex == null) {
-        clean_offender.sex = "Unknown";
-      }
-      return clean_offender;
-    });
-  }
+	cleanOffenders(raw_offenders) {
+		return raw_offenders.map((offender) => {
+			let clean_offender = Object.assign({}, offender);
+			// replace empty and null sex fields with "Unknown"
+			if (clean_offender.sex == "" || clean_offender.sex == null) {
+				clean_offender.sex = "Unknown";
+			}
+			return clean_offender;
+		});
+	}
 }
 
 export default DataManager;
