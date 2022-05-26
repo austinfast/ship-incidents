af93ab8c0277e44cd923fc424464009f67632cbf
mitchthorson <mitch.thorson@protonmail.com>
2022-05-26 15:26:50 -0400

    feat don't reload weapon data

--- src/lib/data/weapons.js
+++ src/lib/data/weapons.js
@@ -1,13 +1,22 @@
 import { getDataURL, getDataFromURL } from "./index.js";
 
-export const weaponData = getWeaponData();
+let weaponData = null;
 
-async function getWeaponData() {
+export function getweaponData() {
+	if (!weaponData) {
+		console.log("weapon data doesn't exist, generating");
+		weaponData = generateVictimData();
+	} else {
+		console.log("weapon data already exists, using copy");
+	}
+	return weaponData;
+}
+
+async function generateWeaponData() {
 	console.log("get weapon data");
 	const dataURL = getDataURL("weapons.json");
 	const rawData = await getDataFromURL(dataURL);
 	return {
-		weapons: rawData
-	}
+		weapons: rawData,
+	};
 }
-
