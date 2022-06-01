57c18cde7218641c0bc2c431983d279e29b5cdc6
mitchthorson <mitch.thorson@protonmail.com>
2022-06-01 18:27:16 -0400

    setup for map, bump up d3 version

--- src/components/charts/IncidentMap.svelte
+++ src/components/charts/IncidentMap.svelte
@@ -1,71 +1,12 @@
 <script>
-  import { onMount } from "svelte";
-  import mapboxgl from "mapbox-gl";
+  import * as topojson from "topojson-client";
   import * as d3 from "d3";
 
   // PROPS
-  export let incidents;
+  export let incidentData;
+  export let popupSlot;
 
-  let el;
-  let width;
-
-  // Configure map
-  function renderMap() {
-    // Mapbox KEY
-    mapboxgl.accessToken = "pk.eyJ1IjoidXNhdG9kYXlncmFwaGljcyIsImEiOiJ0S3BGdndrIn0.5juF5LWz_GRcndian32tZA";
-    
-    // Set circle radius scale based on data
-    let extent = d3.extent(incidents.features.map(feature => feature.properties.victims))
-    let minRadius = width < 600 ? 3 : 5;
-    let maxRadius = width < 600 ? 15 : 25;
-    let circleRadiusScale = d3.scaleSqrt()
-      .domain(extent)
-      .range([minRadius, maxRadius]);
-    
-    incidents.features = incidents.features.map(feature => {
-      let props = Object.assign({}, feature.properties);
-      props.radius = circleRadiusScale(props.victims);
-      return Object.assign({}, feature, {properties: props})
-    })
-
-    let map = new mapboxgl.Map({
-      container: el,
-      bounds: [-24.0,-13.2,20.7,12.8],
-      style: "mapbox://styles/usatodaygraphics/ck0gz14210qh11cq6w8fqzjvz?fresh=true",
-      scrollZoom: false
-    });
-    var nav = new mapboxgl.NavigationControl({showCompass: false});
-    map.addControl(nav, 'top-left');
-    
-    map.on("load", function() {
-      
-      map.addLayer({
-          "id": "incidents",
-          "type": "circle",
-          "source": {
-            "type": "geojson",
-            "data": incidents
-          },
-          paint: {
-            'circle-radius': ['get', 'radius'],
-            'circle-opacity': 0.75,
-            'circle-color': '#d9501e',
-            'circle-stroke-color': '#FFFFFF',
-            'circle-stroke-width': 1
-          }
-      }, "us towns small");
-    });
-  }
-
-  $: if(el && incidents) {
-    width = el.offsetWidth;
-    renderMap();
-  }
 </script>
-<style>
-.incident-map-wrapper {
-  height: 67vw;
-  max-height: 600px;
-}
-</style>
-<div class="incident-map-wrapper" bind:this={el}></div>
+<div class="chart-wrapper"> 
+map go here
+</div>
---
+++ src/incident_map.js
@@ -0,0 +1,19 @@
+import "./style/fonts.css";
+import "./style/index.css";
+import Map from "./components/charts/IncidentMap.svelte";
+import { getIncidentData } from "./lib/data/incidents.js";
+
+const mainEl = document.getElementById("MK-incident_map-embed");
+console.log("Rendering map");
+new Map({
+	target: mainEl,
+	props: {
+		popupSlot: "incident-map",
+		incidentData: getIncidentData()
+	},
+});
+// Set up height resizer for embeds
+if (window.IframeResizer) {
+	const myResizer = new window.IframeResizer(mainEl, 3000);
+	myResizer.watch();
+}
