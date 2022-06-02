705e78f99d3908aab1d7d951fd31bc9ab808eb7b
mitchthorson <mitch.thorson@protonmail.com>
2022-06-01 22:28:07 -0400

    d3 map

---
+++ src/components/MapZoom.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let zoomOut = function (e) {
+		console.log("Zoom out function not implemented");
+	};
+	export let zoomIn = function (e) {
+		console.log("Zoom in function not implemented");
+	};
+</script>
+
+<div class="map-zoom-ui-wrapper">
+	<button on:click={zoomIn}>zoom in</button>
+	<button on:click={zoomOut}>zoom out</button>
+</div>
--- src/components/charts/IncidentMap.svelte
+++ src/components/charts/IncidentMap.svelte
@@ -3,21 +3,33 @@
 	import * as d3 from "d3";
 	import statesTopo from "../../static/states_topo.json";
 	import colors from "../../lib/colors.js";
+	import ZoomControls from "../MapZoom.svelte";
 
 	// PROPS
 	export let incidentData;
 	export let popupSlot;
 
-	const statesGeo = topojson.feature(statesTopo, "states");
+	// Settings
+	let width = 300;
+	const minCircleRadius = width < 400 ? 2 : 3;
+	const maxCircleRadius = width < 400 ? 15 : width < 768 ? 20 : 25;
+
 	let incidents = [];
 	incidentData.then((d) => {
 		incidents = d.incidents;
 	});
-	let width = 300;
-	$: height = width * 0.8;
+	const statesGeo = topojson.feature(statesTopo, "states");
+	let svgEl;
+	let zoomTransform = {
+		x: 0,
+		y: 0,
+		k: 1,
+	};
+	$: svgSelection = d3.select(svgEl);
+	$: height = width * 0.55;
 	$: projection = d3
 		.geoAlbersUsa()
-		.scale(width)
+		.scale(width * 1.1)
 		.translate([width / 2, height / 2]);
 	$: path = d3.geoPath().projection(projection);
 	$: incidentFeatures = incidents.map((d) => {
@@ -28,29 +40,64 @@
 		};
 	});
 	$: console.log(incidentFeatures);
+	$: radiusScale = d3
+		.scaleSqrt()
+		.domain([4, d3.max(incidents, (i) => i.victims)])
+		.range([minCircleRadius, maxCircleRadius]);
+
+	// this receives the magic output of the d3 zoom, we should probably use these values
+	// in some component variables, rather than setting attributes via d3 selections
+	function zoomed(e) {
+		const { transform } = e;
+		console.log(transform);
+		zoomTransform = transform;
+		// mapG.attr("transform", transform);
+		// mapG.attr("stroke-width", 1 / transform.k);
+	}
+
+	// leaning on this d3 zoom magic
+	const zoom = d3.zoom().scaleExtent([1, 8]).on("zoom", zoomed);
+
+	//zoom in handlers:
+	// these basically call a d3 transition on our svg selection and calls the d3 zoom function on the transition
+	// will pass these to the zoom UI component
+	function zoomIn() {
+		console.log("zoomINREAL");
+		svgSelection.transition().call(zoom.scaleBy, 2);
+	}
+	function zoomOut() {
+		console.log("zoomOUTREAL");
+		svgSelection.transition().call(zoom.scaleBy, 0.5);
+	}
 </script>
 
 <div class="chart-wrapper" bind:clientWidth={width}>
-	<svg {width} {height}>
-		<g>
-			{#each statesGeo.features as stateFeature}
-				<path
-					d={path(stateFeature)}
-					fill={colors["grey-light"]}
-					stroke={colors["grey"]} />
-			{/each}
-		</g>
-		<g>
-			{#each incidentFeatures as incidentFeature}
-				{#if incidentFeature.position}
-					<circle
-						cx={incidentFeature.position[0]}
-						cy={incidentFeature.position[1]}
-						fill={colors["orange"]}
-						opacity="0.8"
-						r="5" />
-				{/if}
-			{/each}
+	<svg {width} {height} bind:this={svgEl}>
+		<g
+			class="zoomGroup"
+			transform="translate({zoomTransform.x}, {zoomTransform.y}) scale({zoomTransform.k})">
+			<g>
+				{#each statesGeo.features as stateFeature}
+					<path
+						d={path(stateFeature)}
+						fill={colors["grey-light"]}
+						stroke={colors["grey"]} 
+						stroke-width={1 / zoomTransform.k}/>
+				{/each}
+			</g>
+			<g>
+				{#each incidentFeatures as incidentFeature}
+					{#if incidentFeature.position}
+						<circle
+							cx={incidentFeature.position[0]}
+							cy={incidentFeature.position[1]}
+							fill={colors["orange"]}
+							opacity="0.8"
+							r={radiusScale(incidentFeature.victims)} />
+					{/if}
+				{/each}
+			</g>
 		</g>
 	</svg>
+	<ZoomControls {zoomIn} {zoomOut} />
 </div>
