1091d53401ad2f666115d5ca5f46f514e777a464
Austin Fast <austinfast.news@gmail.com>
2024-07-18 11:33:05 -0400

    Deploy to GitHub Pages

--- src/lib/events.js
+++
@@ -1,7 +0,0 @@
-export function smartResizeListener(cb) {
-  let resizeTimer;
-  window.addEventListener("resize", () => {
-    clearTimeout(resizeTimer);
-    resizeTimer = window.setTimeout(cb, 250);
-  });
-}
--- src/lib/urls.js
+++
@@ -1,8 +0,0 @@
-export function urlFor(urlPath) {
-  const isAbsUrl = urlPath.substring(0, 4) === 'http';
-  const assetPath = process.env.ASSET_PATH || window.location.origin + '/';
-  if (isAbsUrl) {
-    return urlPath;
-  }
-  return `${assetPath}static/${urlPath}`;
-}
