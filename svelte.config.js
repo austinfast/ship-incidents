10b283392230cc38602c474eb32f6adcba2aa683
Austin Fast <austinfast.news@gmail.com>
2024-07-18 12:09:59 -0400

    Update docs files

---
+++ svelte.config.js
@@ -0,0 +1,16 @@
+/*
+import sveltePreprocess from 'svelte-preprocess'
+
+export default {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: sveltePreprocess()
+}
+*/
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/ship-incidents/',
+  // other configurations
+});
